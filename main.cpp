edd2c84ee6e9ddc4747e02c29be730a7c5da502a
287631983 <287631983@qq.com>
2021-01-19 14:08:10 +0800

    静态成员函数

--- main.cpp
+++ main.cpp
@@ -10,44 +10,43 @@ using namespace std;
 
 class Student{
     public:
-        Student(char *name, int age, float score)
-        {
-            this->m_count ++;
-            this->m_name = name;
-            this->m_age = age;
-            this->m_score = score;
-        }
-        void setname(char *name);
-        void setage(int age);
-        void setscore(float score);
+        Student(char *name, int age, float score);
         void show();
+        static int getTotal();
+        static float getPoints();
     private:
-        static int m_count;
+        static int m_total;
+        static float m_points;
         char *m_name;
         int m_age;
         float m_score;
 };
 
-int Student::m_count = 0;
+int Student::m_total = 0;
+float Student::m_points = 0;
 
-void Student::setname(char *name)
+Student::Student(char *name, int age, float score)
 {
+    m_total ++;
+    m_points += score;
     this->m_name = name;
-}
-
-void Student::setage(int age)
-{
     this->m_age = age;
-}
-
-void Student::setscore(float score)
-{
     this->m_score = score;
 }
 
 void Student::show()
 {
-    cout << "index: " << this->m_count << "name:" << this->m_name << " age:" << this->m_age << " score:" << this->m_score << endl;
+    cout << m_name << "的年龄是" << m_age << "岁,成绩是" << m_score << "分" << endl;
+}
+
+int Student::getTotal()
+{
+    return m_total;
+}
+
+float Student::getPoints()
+{
+    return m_points;
 }
 
 int main(int argc, char **argv)
@@ -55,4 +54,10 @@ int main(int argc, char **argv)
     (new Student("小明", 18, 95.1))->show();
     (new Student("小刚", 18, 94.9))->show();
     (new Student("小花", 18, 91.5))->show();
+
+    int total = Student::getTotal();
+    float points = Student::getPoints();
+    cout << "当前共有" << total << "名学生,总成绩是" << points << endl;
+
+    return 0;
 }
